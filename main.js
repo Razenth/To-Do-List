6d131cf96b0595eec75c93c07c5da04dbbaa677a
Santi <santiago.gonzalezagudelo@gmail.com>
2023-03-24 13:26:43 -0500

    Preparacion Exposicion

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
-const ContainerTodo=document.querySelector('#contenedorTodito')
-
+const ContainerTodo=document.querySelector('#contenedorTodito') //Traemos nuestro contenedor principal de HTML (Luego lo utilizaremos)
+// const otro=document.querySelectorAll  //Crea un HTML collection, donde 
 
 // Estableciendo el HTML
-const headerCont=document.createElement('div')
-const header=document.createElement('header')
-header.setAttribute('class','title')
-header.textContent='To do List'
-headerCont.append(header)
-const inputs=document.createElement('div')
-inputs.className='inputs'
+const headerContenedor=document.createElement('div') //Creamos el contenedor del header o titulo en este caso un DIV
+
+const header=document.createElement('header') //Creamos un elemento header, donde tendrémos nuestro título
+
+header.setAttribute('class','title') //Al header le agregamos el atributo, donde separamos por coma el identificador con el nombre = ('class', 'nombre')
+
+header.textContent='To do List'  //Agregamos texto al header que creamos anteriormente
+
+headerContenedor.append(header) //Y agregamos el header al contenedor del Header
+
+const inputs=document.createElement('div') //Ahora creamos un div, el cual contendrá el input y el botón de agregar
+inputs.className='inputs' //Le asignamos el nombre a la clase, una forma distinta al 'setAttribute'
+
 const entrada=document.createElement('input')
 entrada.setAttribute('class','entrada') 
 entrada.placeholder='Escriba su por hacer aquí'
+
 const agregar=document.createElement('button')
 agregar.setAttribute('class', 'agregar')
 agregar.textContent='+'
+
 inputs.append(entrada,agregar)
 const elementos=document.createElement('div')
 elementos.setAttribute('class', 'lista')
 
 // Agregando todo al HTML
-ContainerTodo.append(headerCont)
+ContainerTodo.append(headerContenedor)
 ContainerTodo.append(inputs)
 ContainerTodo.append(elementos)
+
+const confirmar = document.createElement('div')
+const tarea=document.createElement('div')
+const borrarTarea=document.createElement('div')
+const opciones=document.createElement('div')
 
 
 // Botón Agregar
@@ -30,15 +43,27 @@ agregar.addEventListener('click',()=>{
     if(entrada.value==''){
         alert('Debe ingresar contenido en su tarea')
     }else{
-    const tarea=document.createElement('div')
     tarea.className='tarea'
     tarea.textContent=`${entrada.value}`
     entrada.value=''
-    const borrarTarea=document.createElement('div')
+
+    confirmar.className='bi bi-check'
+    confirmar.onmouseenter= function (e) {
+        e.target.style.color = 'green';
+    }
+    confirmar.onmouseout= function (e) {
+        e.target.style.color = 'white';
+    }
+
     borrarTarea.className='bi bi-trash3'
 
-    tarea.appendChild(borrarTarea)
-    elementos.append(tarea)
+
+    opciones.append(confirmar, borrarTarea)
+    opciones.style.display='flex'
+    opciones.style.gap='15px'
+
+    tarea.appendChild(opciones)
+    elementos.appendChild(tarea)
     console.log(entrada.value)
     }
 })
@@ -46,6 +71,12 @@ agregar.addEventListener('click',()=>{
 // Botón Borrar
 elementos.addEventListener('click',(e)=>{
     if(e.target.className=="bi bi-trash3"){
-       elementos.removeChild(e.target.parentElement)
+       elementos.removeChild(e.target.parentElement.parentElement)
     }
+})
+
+//Botón Check
+
+confirmar.addEventListener('click', ()=>{
+    tarea.style.backgroundColor = 'green'
 })
